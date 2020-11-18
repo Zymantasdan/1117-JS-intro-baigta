7587a32e2250e9088f7aac83ca771fd953e7b97b
z.dansevicius <z.dansevicius@gmail.com>
2020-11-18 08:31:06 +0200

    info sutrumpinta

---
+++ js/info.js
@@ -0,0 +1,25 @@
+// Sutrumpinimai
+
+a = a + b;
+a += b;
+
+a = a - b;
+a -= b;
+
+a = a * b;
+a *= b;
+
+a = a / b;
+a /= b;
+
+a = a % b;
+a %= b;
+
+
+i = i + 1;
+i++;
+++i;
+
+i = i - 1;
+i--;
+--i;
